abfc17b7551ca2915cdd4f7f975988582e9e7b97
TKmatsu2008 <fhenixeaglu1st@icloud.com>
2022-07-15 21:47:57 +0900

    画像アップロード機能の追加

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
+/* eslint-disable */
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.json())
 
-
-//CORSポリシーを無効にしている。
+// CORSポリシーを無効にしている。
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
