d29ed5d874a3fa215fd58a79e159050e6ce1a93f
Yusuke Ise <shosuke447@gmail.com>
2022-07-15 21:19:33 +0900

    画像ファイルを受け渡せるように変更

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
+// eslint-disable-next-line
+/* eslint-disable */ 
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -17,16 +20,32 @@ app.get('/api', function(req, res) {
   var {PythonShell} = require('python-shell');
   var pyshell = new PythonShell('./backend/sample.py');  
   console.log("req")
-  console.log(req.query.dat) //フロントエンドから受け取ったデータをconsole.logしている。
+  // console.log(req.query.image) //フロントエンドから受け取ったデータをconsole.logしている。
+  // res.send({
+  //   resultImages: [req.query.image, req.query.image]
+  // })
 
-  pyshell.send(req.query.dat); //本コードからpythonコードに'req.query.dat'を入力データとして提供する 
+  const options = {
+    mode: 'json',
+    args: [req.query.image],
+  }
+
+  PythonShell.run('./backend/sample.py', options, function(err, data){
+    if(err){
+      console.log('err')
+    }else{
+      pyshell.send(data)
+    }
+  })
+
+  // pyshell.send(req.query.image); //本コードからpythonコードに'req.query.dat'を入力データとして提供する 
 
   //pythonコード実施後にpythonから本コードにデータが引き渡される。
   pyshell.on('message',  function (data) {
-    console.log("return data")
-    console.log(data)
+    console.log('return data')
+    console.log(typeof(data), data)
     res.send({
-      message: data   //pythonで実施した演算結果をフロントエンドに返している。
+      resultImages: data[0]['resultImages']   //pythonで実施した演算結果をフロントエンドに返している。
     })
   })
 
--- src/components/Top.vue
+++ src/components/Top.vue
@@ -4,6 +4,7 @@
     <h1>デモ画面</h1>
     <input type="button" value="移動" @click="goNewTask()"> <br>
     <input type="number" v-model="message"><input type="button" value="取得" @click="getdata()">
+    <input type="button" value="画像" @click="getImage()">
     <p> <font size="2"> 入力データ :{{ $data.message }} </font> </p>
     <p> <font size="2"> 出力データ :{{ $data.result }} </font> </p>
     <p> <font size="2"> 状態 :{{ $data.state }} </font> </p>
@@ -15,6 +16,7 @@
 // eslint-disable-next-line
 /* eslint-disable */ 
 import * as d3 from 'd3'  //有効にする
+import logo from '@/assets/logo.png'
 
 export default {
   name: 'Top',
@@ -22,7 +24,9 @@ export default {
     return { 
         message:'',  //入力データを格納する変数。
         result :'',  //演算結果を格納する変数。
-        state:"wait" //現在の状況を格納する変数。
+        state:"wait", //現在の状況を格納する変数。
+        image: '',
+        resultImages: []
     }
   },
   methods: {
@@ -39,7 +43,25 @@ export default {
           this.state="ERROR"
           }.bind(this))
         .finally(function(){
-          }.bind(this))}
+          }.bind(this))
+    },
+    getImage:function(){
+      this.state="getting data"
+      const sdata = new FormData()
+      this.image = logo
+      sdata.append('image', this.image)
+      this.$axios.get('http://localhost:3000/api', {params: {image: this.image}})
+        .then(function(response){
+          this.result= response.data.resultImages
+          console.log(this.result.resultImages)
+          this.state="done"    
+          }.bind(this))  //Promise処理を行う場合は.bind(this)が必要
+        .catch(function(error){  //バックエンドからエラーが返却された場合に行う処理について
+          this.state="ERROR"
+          }.bind(this))
+        .finally(function(){
+          }.bind(this))
+    }
   } 
 }
 
